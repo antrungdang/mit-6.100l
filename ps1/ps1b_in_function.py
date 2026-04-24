fe052f0dbdba92371ce8339d93202a4f742fac75
atd <antrungdang.dev@gmail.com>
2026-04-24 09:26:26 +0700

    feat: add problem set 2 part B solution

---
+++ ps1/ps1b_in_function.py
@@ -0,0 +1,25 @@
+def part_b(yearly_salary, portion_saved, cost_of_dream_home, semi_annual_raise):
+	#########################################################################
+	portion_down_payment = 0.25 # percentage of the total cost needed for a down payment
+	amount_saved = 0            # amount that you have saved thus far
+	r = 0.05                    # annual rate of return
+	months = 0                  # number of months required to save for the down payment
+	
+	cost_of_down_payment = cost_of_dream_home * portion_down_payment
+	monthly_saved = yearly_salary / 12 * portion_saved
+	
+	###############################################################################################
+	## Determine how many months it would take to get the down payment for your dream home below ## 
+	###############################################################################################
+	while amount_saved <= cost_of_down_payment:
+	    amount_saved += amount_saved * (r / 12)
+	    amount_saved += monthly_saved
+	    months += 1
+	
+	    if months % 6 == 0:
+	        yearly_salary += yearly_salary * semi_annual_raise
+	        monthly_saved = yearly_salary / 12 * portion_saved
+	
+	print(f"Number of months: {months}")
+	
+	return months
