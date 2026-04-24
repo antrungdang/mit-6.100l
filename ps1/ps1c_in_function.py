3ffcdd03821d3bfbe02df7da741989b0d47cc9b4
atd <antrungdang.dev@gmail.com>
2026-04-24 10:28:09 +0700

    feat: add problem set 3 part C solution

---
+++ ps1/ps1c_in_function.py
@@ -0,0 +1,37 @@
+def part_c(initial_deposit):
+	#########################################################################
+	cost_of_down_payment = 200000   # 800000 * 25%
+	months = 36
+	
+	epsilon = 100
+	steps = 1
+	low = 0
+	high = 1
+	r = (high + low) / 2
+	
+	amount_saved = initial_deposit * (1 + r / 12) ** months
+	max_amount_saved = initial_deposit * (1 + high / 12) ** months
+	
+	##################################################################################################
+	## Determine the lowest rate of return needed to get the down payment for your dream home below ##
+	##################################################################################################
+	if max_amount_saved < cost_of_down_payment:
+	    r = None
+	    steps = 0
+	
+	else:
+	    while abs(amount_saved - cost_of_down_payment) > epsilon:
+	        if amount_saved < cost_of_down_payment:
+	            low = r
+	        else:
+	            high = r
+	
+	        r = (high + low) / 2
+	        amount_saved = initial_deposit * (1 + r / 12) ** months
+	
+	        steps += 1
+	
+	print(f"Best savings rate: {r}")
+	print(f"Steps in bisection search: {steps}")
+	
+	return r, steps
